3a24db5145859a25096df07be9484baa6e661596
lk <1205547998@qq.com>
2022-07-03 11:55:39 +0800

    测试

---
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -0,0 +1,17 @@
+package com.nowcoder.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication
+public class CommunityApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
